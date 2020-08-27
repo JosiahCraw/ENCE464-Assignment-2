a9b00cc56bbf30c0428ad376a2674ef387f3a70b
Michael Hayes <michael.hayes@canterbury.ac.nz>
2020-08-27 22:22:53 +1200

    Add Vnbound

--- src/poisson.cpp
+++ src/poisson.cpp
@@ -2,6 +2,7 @@
 /// boundary conditions on each face.
 /// \param source is a pointer to a flattened 3-D array for the source function
 /// \param potential is a pointer to a flattened 3-D array for the calculated potential
+/// \param Vbound is the potential on the boundary
 /// \param xsize is the number of elements in the x-direction
 /// \param ysize is the number of elements in the y-direction
 /// \param zsize is the number of elements in the z-direction
@@ -10,8 +11,10 @@
 /// \param numcores is the number of CPU cores to use.  If 0, an optimal number is chosen
 void poisson_dirichlet (double * __restrict__ source,
                         double * __restrict__ potential,
+                        double Vbound,
                         unsigned int xsize, unsigned int ysize, unsigned int zsize, double delta,
                         unsigned int numiters, unsigned int numcores)
 {
     // source[i, j, k] is accessed with source[((k * ysize) + j) * xsize + i]
+    // potential[i, j, k] is accessed with potential[((k * ysize) + j) * xsize + i]    
 }
--- src/poisson.hpp
+++ src/poisson.hpp
@@ -3,7 +3,9 @@
 
 // Solve Poisson's equation for a rectangular box with Dirichlet
 // boundary conditions on each face.
-void poisson_dirichlet (double *__restrict__ source, double *__restrict__ potential,
+void poisson_dirichlet (double *__restrict__ source,
+                        double *__restrict__ potential,
+                        double Vbound,
                         unsigned int xsize, unsigned int ysize, unsigned int zsize,
                         double delta, unsigned int maxiters, unsigned int numcores);
 #endif
