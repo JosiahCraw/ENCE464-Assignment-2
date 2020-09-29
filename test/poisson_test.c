57033ac60e1c3989583a8c7a668652fa1e1c341c
Jos Craw <jos@joscraw.net>
2020-09-29 22:54:08 +1300

    Changed the check to hash to reduce memory complexity

--- test/poisson_test.c
+++ test/poisson_test.c
@@ -1,15 +1,31 @@
 /// \brief Test program for solving Poisson's equation using Jacobi relaxation
-/// \author M. P. Hayes UCECE
+/// \author M. P. Hayes UCECE, Significantly Modifided by, Jos Craw 2020
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include "example/demo_poisson.h"
-#include "../src/poisson.h"
+#include <openssl/md5.h>
+#include <string.h>
+
+/*
+ * Declear the example function and user function as
+ * extern as they will be included at compile time
+ */
+extern void demo_poisson_dirichlet (double *__restrict__ source,
+                        double *__restrict__ potential,
+                        double Vbound,
+                        unsigned int xsize, unsigned int ysize, unsigned int zsize,
+                        double delta, unsigned int maxiters, unsigned int numcores);
+
+extern void poisson_dirichlet (double *__restrict__ source,
+                        double *__restrict__ potential,
+                        double Vbound,
+                        unsigned int xsize, unsigned int ysize, unsigned int zsize,
+                        double delta, unsigned int maxiters, unsigned int numcores);
 
 int main (int argc, char *argv[])
 {
-    double *demo_potential;
+    // Init variables
     double *source;
     double *potential;
 
@@ -39,21 +55,47 @@ int main (int argc, char *argv[])
     else
         numcores = 0;
 
+    // Set source and potential to empty intially
     source = (double *)calloc(xsize * ysize * zsize, sizeof(*source));
-    potential = (double *)calloc(xsize * ysize * zsize, sizeof(*potential));
-    demo_potential = (double *)calloc(xsize * ysize * zsize, sizeof(*potential));
+    potential = (double *)calloc(xsize * ysize * zsize + 1, sizeof(*potential));
 
     source[((zsize / 2 * ysize) + ysize / 2) * xsize + xsize / 2] = 1.0;    
     
+    // Run the users implementation
     poisson_dirichlet(source, potential, 0, xsize, ysize, zsize, delta,
                       numiters, numcores);
 
-    demo_poisson_dirichlet(source, demo_potential, 0, xsize, ysize, zsize, delta,
+    // The size of the potential array, this is used later
+    size_t potential_size = xsize * ysize * zsize * sizeof(*source);
+
+    // Initalise the MD5 Hash
+    MD5_CTX md5;
+    MD5_CTX md5_demo;
+
+    // Init the MD5 Hash
+    MD5_Init(&md5);
+    MD5_Init(&md5_demo);
+
+    // Hash the First run
+    char hash[MD5_DIGEST_LENGTH];
+    MD5_Update(&md5, potential, potential_size);
+    MD5_Final(hash, &md5);
+
+    // Reset the variables
+    memset(potential, 0, potential_size);
+    memset(source, 0, potential_size);
+
+    // Run the Demo implementation
+    demo_poisson_dirichlet(source, potential, 0, xsize, ysize, zsize, delta,
                       numiters, numcores);
-    
-    for (int i=0; i<(xsize * ysize * zsize); i++) {
-        assert(*(potential+i) == *(demo_potential+i));
-    } 
+
+    // Hash the second run
+    char demo_hash[MD5_DIGEST_LENGTH];
+    MD5_Update(&md5_demo, potential, potential_size);
+    MD5_Final(demo_hash, &md5_demo);
+
+    // Check the hashes are equal
+    assert(!strcmp(hash, demo_hash));
 
     return 0;
 }
